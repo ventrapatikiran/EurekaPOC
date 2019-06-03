381252e472be564f88a245c32f7420474f30b009
kiran.kumar <kiran.kumar@care.com>
2019-06-03 21:17:42 +0530

    Initial Commit

---
+++ src/main/java/com/example/EurekaPOC/EurekaPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.EurekaPOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaPocApplication.class, args);
+	}
+
+}
